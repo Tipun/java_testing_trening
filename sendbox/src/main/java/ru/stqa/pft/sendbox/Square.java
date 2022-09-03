0e760ff20cb857b26198f8e7f3c34eaf043eaace
Максим <mtipun@mail.ru>
2022-09-03 18:13:03 +0300

    первая программа

---
+++ sendbox/src/main/java/ru/stqa/pft/sendbox/Square.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sendbox;
+
+public class Square {
+
+    public double l;
+
+    public Square(double l){
+        this.l = l;
+    }
+
+    public double area(){
+        return this.l + this.l;
+    }
+}
