888b2b59bedae232b44fbb9f8c9c0fe837d1ec2b
Максим <mtipun@mail.ru>
2022-09-04 12:57:56 +0300

    первый тест идет

--- sendbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sendbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -9,6 +9,6 @@ public class SquareTests {
     @Test
     public void testArea(){
         Square s = new Square(5.0);
-        Assert.assertEquals(s.area(),25.0);
+        Assert.assertEquals(s.area(),24.0);
     }
 }
